3ebec558cc585692e3b9cb769f4cb3670fb7ba90
Sebastiano Caccaro <sebastianocaccaro@gmail.com>
2017-12-04 18:13:26 +0100

    Riordinato codice La conversione da stringa a basex è ora implicita attraverso il construttore basex(const string& );

--- basx.h
+++ basx.h
@@ -5,16 +5,26 @@ class basex{
 private:
     T raw_number;//solo tipi num
     int base;
+
 public:
-    virtual basex(T=0,int=10);//vedere pos virtual
+        
+    
+    //Costruttori
+    basex(T=0,int=10);
+    basex(const string& );  //Costruttore attraverso una stringa
+                            //Funge anche da conversione implicita da stringa
+
+    //Overload di operatori
     basex operator+(const basex& )const;
     basex operator*(const basex& )const;
     basex operator/(const basex& )const;
     basex operator-(const basex& )const;
-    operator string()const;
-    basex toBase();
-    static T toTen(const basex& n);
     
+    //Conversioni
+    operator string()const; //Converto *this a stringa
+    T toTen(const basex& ); //Converto *this in un numero T in base 10
+    /*  Sarà necessario dichiarare dei metodi di conversione per i tipi che T può assumere
+        = pinga in culo*/
 
 
 //overload output
