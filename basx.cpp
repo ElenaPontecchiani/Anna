22636847fd88c63676e1c1e6f40a6723a798c980
ElenaPontecchiani <ele.pontecchiani@gmail.com>
2017-12-06 11:27:57 +0100

    lol aggiunta statiche di conversione pt pv modifica costruttore tramite string aggiunta overload assegnazione aggiunta void changeBase tolto toTen

---
+++ basx.cpp
@@ -0,0 +1,21 @@
+#include "basx.h"
+
+template<class T>
+basex<T>::basex(T t, int b): raw_number(t), base(b) {
+    //se base>1 ok altrementi err
+};
+
+template<class T>
+basex<T>::basex(const string& s,int b):  {
+    //se base>1 ok altrementi err
+    //controllo num<0
+    //chiamata string->T
+};
+//conversione da string a base x
+
+
+
+
+
+
+
--- basx.h
+++ basx.h
@@ -3,29 +3,32 @@
 template<class T>
 class basex{
 private:
-    T raw_number;//solo tipi num
+    T raw_number;//solo tipi num e inbase 10
     int base;
+    static int charToInt(const char&);//conversione char->int
+    static char intToChar(const int&);//conversione int->char
+    static T stingToNum(const string&,const int&);//string->T
 
 public:
         
-    
     //Costruttori
     basex(T=0,int=10);
-    basex(const string& );  //Costruttore attraverso una stringa
+    basex(const string&,int=10);  //Costruttore attraverso una stringa
                             //Funge anche da conversione implicita da stringa
 
-    //Overload di operatori
-    basex operator+(const basex& )const;
-    basex operator*(const basex& )const;
-    basex operator/(const basex& )const;
-    basex operator-(const basex& )const;
-    
+    //Overload di operatori, base ritorno operando più a sx, this
+    basex operator+(const basex&)const;
+    basex operator*(const basex&)const;
+    basex operator/(const basex&)const;
+    basex operator-(const basex&)const;
+    basex& operator=(const basex&);//copia valore ma non base
+
+
     //Conversioni
     operator string()const; //Converto *this a stringa
-    T toTen(const basex& ); //Converto *this in un numero T in base 10
     /*  Sarà necessario dichiarare dei metodi di conversione per i tipi che T può assumere
         = pinga in culo*/
-
+    void changeBase(int base);
 
 //overload output
 
