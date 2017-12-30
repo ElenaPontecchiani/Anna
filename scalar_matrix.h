0c07ba16a2519f0736c56a74db9b6ef07168f206
Sebastiano Caccaro <sebastianocaccaro@gmail.com>
2017-12-30 19:19:29 +0100

    Rimaneggiato scalar_matrix

--- scalar_matrix.h
+++ scalar_matrix.h
@@ -4,20 +4,20 @@
 #include "diagonal_matrix.h" //de ve includere base o il contrario?
 
 template<class T>
-class scalar_matrix: public diagonal_matrix{
+class scalar_matrix: public diagonal_matrix<T>{
 private: 
 
 public:
+    
+    scalar_matrix(int, const T& =0);
+    virtual T Det ()const;
 
+    void setDiag(const T& t);
 
-    scalar_matrix(const T&=0,const T&=0);//in input el diag ()che è sempre uguale e dim==n el diag
-    scalar_matrix(const T&=0,const T&=0,const int&=0);
-    T Determinante ()const;
-
-    scalar_matrix<T> operator+(const scalar_matrix<T>&)const;
+    /*scalar_matrix<T> operator+(const scalar_matrix<T>&)const;
     scalar_matrix<T> operator-(const scalar_matrix<T>&)const;
     scalar_matrix<T> operator*(const scalar_matrix<T>&)const;
-    scalar_matrix<T> operator*(const T&)const;
+    scalar_matrix<T> operator*(const T&)const;*/
 };
 
 
@@ -27,106 +27,26 @@ public:
 //////////////////////////////
 
 template <class T>
-scalar_matrix<T>::scalar_matrix(const int& dim):
-    *this(diagonal_matrix<T>(dim,dim)){
-    } 
-
-
-template <class T>
-scalar_matrix<T>::scalar_matrix(const int& dim,const int& el)://secondo costruttore che prende in input anche el da mettere su diag
-    *this(diagonal_matrix<T>(dim,dim)){
-       for(int i=0;i<dim^2;i++){
-        if(onDiagonal(dim,i)){//se è sulla diagonale mettere el?
-          raw_matrix[i]=el;
-        }
-        else{raw_matrix[i]=0;}
-      }
-    }
+scalar_matrix<T>::scalar_matrix(int dim, const T& t): diagonal_matrix<T>(dim,t){}
 
     
 //////////////////////////////
 //   O P E R A Z I O N I    //
 //////////////////////////////
 
+#include <math.h>
 template<class T>
-T scalar_matrix<T>::Determinante()const{
-return raw_matrix[0]*h;
+T scalar_matrix<T>::Det()const{
+  return pow((*this)[0],this->getL());
 }
-
-
-
-//////////////////////////////
-//   OP  O V E R L O A D    //
-//////////////////////////////
-
-#include "basxMath.h"
-template <class T>
-diagonal_matrix<T> diagonal_matrix<T>::operator+(const diagonal_matrix<T>& m)const{
-  if(!(*this.dimensions(m))){
-    //Throw///
-    std::cout << "Non sommabile vezz" << std::endl;
-  }
-  diagonal_matrix<T> temp(h,l);
-  T t=raw_matrix[0]+m.raw_matrix[0];//el tt uguali da mettere sulla diag risultante
-  for(int i = 0; i < l*h; i++){
-    if(onDiagonal(h^2,i))
-    {temp[i] = t;}
-    else{temp[i] =0;}
-}
-  return temp;
-}
-
-
 
 template <class T>
-diagonal_matrix<T> diagonal_matrix<T>::operator-(const diagonal_matrix<T>& m)const{
-  if(!(*this.dimensions(m))){
-    //Throw///
-    std::cout << "Non sommabile vezz" << std::endl;
-  }
-  diagonal_matrix<T> temp(h,l);
-  T t=raw_matrix[0]-m.raw_matrix[0];//el tt uguali da mettere sulla diag risultante
-  for(int i = 0; i < l*h; i++){
-    if(onDiagonal(h^2,i))
-    {temp[i] = t;}
-    else{temp[i] =0;}
-}
-  return temp;
+void scalar_matrix<T>::setDiag(const T& t){
+  for(int i = 0; i < this->getL(); i++)
+    (*this)[i + this->getL()*i] = t;
 }
 
 
-
-template <class T>
-scalar_matrix<T> scalar_matrix<T>::operator*(const T& w)const{
-  if(!(*this.dimensions(m))){
-    //Throw///
-    std::cout << "Non sommabile vezz" << std::endl;
-  }
- scalar_matrix<T> temp(h,l);
-  T t=raw_matrix[0]*w;//el tt uguali da mettere sulla diag risultante
-  for(int i = 0; i < l*h; i++){
-    if(onDiagonal(h^2,i))
-    {temp[i] = t;}
-    else{temp[i] =0;}
-}
-  return temp;
-}
-
-
-template <class T>
-scalar_matrix<T> scalar_matrix<T>::operator*(const scalar_matrix<T>& m)const{
-  iff(!(*this.dimensions(m))){//il metodo si può invocare anche qui tanto sono quadrate
-    std::cout << "dim sbagliate";
-  }
-  scalar_matrix<T> temp(h,h);
-  T t=raw_matrix[0]*m.raw_matrix[0];
-  for(int i = 0; i < h^2; i++){
-    if(onDiagonal(h^2,i))
-    {temp[i] =t;}
-    else {temp[i] =0;}
-    }
-  return temp;
-}
 
 
 
